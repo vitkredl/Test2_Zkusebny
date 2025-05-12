38f85822f3a4b8fbb6a958e707ff1edce901901a
vitkr <vitkr@VITNB>
2025-05-12 11:54:44 +0200

    Přidejte soubory projektu.

---
+++ Test2_Zkusebny/Models/RegistraceUzivatel.cs
@@ -0,0 +1,19 @@
+﻿namespace Test2_Zkusebny.Models
+{
+    public class RegistraceUzivatel
+    {
+        public string Jmeno { get; set; } = "";
+        public string Email { get; set; } = "";
+        public DateTime DatumRegistrace { get; set; } = DateTime.Now;
+        public decimal Vlozne { get; set; }
+        public int StavZaplaceni { get; set; }
+    }
+
+
+    public enum StavZaplaceni
+    {
+        Nezaplaceno = 0,
+        Zaplaceno = 1,
+        Zruseno = 2
+    }
+}
